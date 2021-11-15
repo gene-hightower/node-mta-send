4068e7b0afe26a81e8e36419124b55c78684ea1e
Gene Hightower <gene@digilicious.com>
2021-11-14 19:29:19 -0800

    use instanceOfNodeError, make pretty

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,29 @@
 const SMTPConnection = require("nodemailer/lib/smtp-connection");
 const _ = require("lodash");
 const dns = require("dns");
-const isIp = require("is-ip");
 const pify = require("pify");
 const smtpAddressParser = require("smtp-address-parser");
 
-import {EventEmitter} from 'events';
+import { EventEmitter } from "events";
 
-// function isError(error: any): error is NodeJS.ErrnoException { return error instanceof Error; }
+/**
+ * A typeguarded version of `instanceof Error` for NodeJS.
+ * @author Joseph JDBar Barron
+ * @link https://dev.to/jdbar
+ */
+export function instanceOfNodeError<T extends new (...args: any) => Error>(
+    value: Error,
+    errorType: T
+): value is InstanceType<T> & NodeJS.ErrnoException {
+    return value instanceof errorType;
+}
 
-function wrapErrorEvent(target: EventEmitter, other: () => Promise<any>, logger: any) : Promise<unknown>
-{
+function wrapErrorEvent(target: EventEmitter, other: () => Promise<any>, logger: any): Promise<unknown> {
     return new Promise(function (resolve, reject) {
         target.on("error", (err) => {
             logger.error(`Captured a socket error! ${err}`);
             reject(err);
         });
-
         other()
             .then((r) => resolve(r))
             .catch((e) => reject(e));
@@ -29,39 +36,37 @@ function wrapErrorEvent(target: EventEmitter, other: () => Promise<any>, logger:
  * Returns the ordered list of Mail eXchangers for a domain, including
  * an implicit record if no others found in DNS.
  */
-async function getMXs(domain: string)
-{
+async function getMXs(domain: string) {
     try {
         const mx = await dns.promises.resolveMx(domain);
         return _.sortBy(mx, ["priority", Math.random]);
-    } catch (e: any) {
-
-            if (e.code === "ENODATA" || e.code === "ENOTFOUND") {
+    } catch (err_mx: any) {
+        if (instanceOfNodeError(err_mx, Error)) {
+            if (err_mx.code === "ENODATA" || err_mx.code === "ENOTFOUND") {
                 try {
                     await Promise.any([dns.promises.resolve4(domain), dns.promises.resolve6(domain)]);
                     // RFC-5321 section 5.1. "implicit MX"
                     return [{ priority: 0, exchange: domain }];
+                } catch (e_addr: any) {
+                    if (instanceOfNodeError(e_addr, Error)) {
+                        if (e_addr.code === "ENODATA" || e_addr.code === "ENOTFOUND") {
+                            // No 'A' or 'AAAA' record
+                            return [];
+                        }
+                    } else {
+                        throw e_addr; // anything else, including code==="ETIMEOUT"
+                    }
                 }
-                catch (e) {
-                    // No 'A' record
-                    return [];
-                }
-            } else if (e.errno === "ETIMEOUT") {
-                return [];
             } else {
-                throw e;
+                throw err_mx; // anything else, including code==="ETIMEOUT"
             }
-
+        } else {
+            throw err_mx; // throwing non Error should never happen
+        }
     }
 }
 
-export async function sendOutgoingEmail(name: string,
-                                        mailFrom: string,
-                                        rcptTo: string,
-                                        content: any,
-                                        logger : any,
-                                        tls : any)
-{
+export async function sendEmail(name: string, mailFrom: string, rcptTo: string, content: any, logger: any, tls: any) {
     const from_parsed = smtpAddressParser.parse(mailFrom);
     const to_parsed = smtpAddressParser.parse(rcptTo);
 
@@ -77,14 +82,14 @@ export async function sendOutgoingEmail(name: string,
     const mxRecords = await getMXs(to_parsed.domainPart.DomainName);
 
     if (mxRecords.length === 0) {
-        var err: {[k: string]: any} = new Error("No MX (or A or AAAA) record");
+        var err: { [k: string]: any } = new Error("No MX (or A or AAAA) record");
         err.response = "No MX or A or AAAA";
         err.responseCode = 500;
         throw err;
     }
 
     if (mxRecords.length === 1 && mxRecords[0].priority === 0 && mxRecords[0].exchange === "") {
-        var err: {[k: string]: any} = new Error("Null MX record");
+        var err: { [k: string]: any } = new Error("Null MX record");
         err.response = "Null MX";
         err.responseCode = 500;
         throw err;
@@ -97,20 +102,16 @@ export async function sendOutgoingEmail(name: string,
             port: 25,
             host: host,
             name: name,
-            secure: false,
+            secure: false, // connect in plain-text
+            opportunisticTLS: true, // upgrade using STARTTLS
+            socketTimeout: 30 * 1000, // 30 seconds
             transactionLog: true,
-            opportunisticTLS: true,
-            socketTimeout: 2 * 60 * 1000,
             tls,
         });
 
         const connect = pify(connection.connect.bind(connection));
         const send = pify(connection.send.bind(connection));
         try {
-            if (!isIp(host)) {
-                // attempt to resolve host early, to avoid connecting to non existing host
-                await dns.promises.resolve4(host);
-            }
             const result = await wrapErrorEvent(
                 connection,
                 async () => {
@@ -125,14 +126,12 @@ export async function sendOutgoingEmail(name: string,
             );
             connection.quit();
             return result;
-        } catch (e: any) {
-            const error_message = `Error in outbound from ${envelope.from} to ${envelope.to}: ${JSON.stringify(e)}`;
-            logger.debug(error_message);
+        } catch (e) {
+            logger.debug(`${JSON.stringify(e)}`);
         }
 
         connection.quit();
     }
- 
-   throw new Error("Could not deliver message.");
-}
 
+    throw new Error("Could not deliver message.");
+}
